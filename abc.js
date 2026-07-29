224d811aaeacf39e19a15a90f43910a39931158e
onlyno999 <105115958+onlyno999@users.noreply.github.com>
2026-07-29 20:43:01 +0800

    Update abc.js

--- abc.js
+++ abc.js
@@ -4,13 +4,13 @@ import { connect } from 'cloudflare:sockets';
 let UUID = "bee9ac63-20ea-4b0b-876a-09831e5f755a";
 
 // 1. 内置 ProxyIP 备用代理 (直连失败后回退，格式: "ip:port")
-const PROXYIP = "wok.woxxxxxx.nyc.mn";  
+const PROXYIP = "??????????";  
 
 // 2. 内置 SOCKS5 / HTTP 备用代理 (直连失败后回退，格式: "user:pass@host:port" 或 "host:port")
-const SOCKS5 = ""; 
+const SOCKS5 = "!!!!!!!!!!!!"; 
 
 // 3. 内置强制全局代理 (若填写则跳过直连，全量走此代理，格式: "socks5://..." 或 "http://...")
-const SOCKS5_GLOBAL = ""; 
+const SOCKS5_GLOBAL = ",,,,,,,,,,,"; 
 
 // ========================================================
 
